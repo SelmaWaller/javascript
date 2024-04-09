8ee60a5ecbcc3509c12655cd59efee354e63ffac
Selma <selma.waller@glasspaper.no>
2024-04-09 12:02:21 +0200

    local api/server routes

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
+<script setup>
+ //const { data } = await useFetch('/api/example?name=mario', {
+ //  method: 'post',
+ //  body: { age: 30 }
+ //})
+ 
+ const { data } = await useFetch('/api/server-routes')
 </script>
 
 <template>
   <div>
     <h2>About</h2>
+    <div>
+      {{ data }}
+    </div>
   </div>
 </template>
 
